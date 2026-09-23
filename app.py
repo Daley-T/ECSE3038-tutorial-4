752eb4d283fa2f936850dc2d1d564d19ee26d6d8
Daley-T <tivoniadaley123@gmail.com>
2026-09-23 16:09:11 -0400

    task 1: update device

--- app.py
+++ app.py
@@ -38,3 +38,13 @@ def create_device(device: Device):
     new_device = device.model_dump()
     readings.append(new_device)
     return new_device
+
+@app.put("/devices/{name}")
+def update_device(name:str ,device:Device):
+    for index, new_device in enumerate(readings):
+        if new_device["name"] == name:
+            readings[index] = device.model_dump()
+            return readings[index]
+    raise HTTPException(status_code=404, detail = name + " does not exist")
+
+    
